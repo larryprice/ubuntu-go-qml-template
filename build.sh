02314bf2218de3cbf7984c5e55068ecd7e0151d6
Larry Price <larry.price.dev@gmail.com>
2016-07-05 21:45:48 -0400

    builds and runs locally

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-DIR=$(dirname $(readlink -f "$0"))
-export GOPATH=$DIR
-
-go get -d -u gopkg.in/qml.v1
-go install -v -x ubuntu-go-qml-template
-cp bin/ubuntu-go-qml-template $DIR
+go build -o bin/beer-buddy -v -x src/main.go
